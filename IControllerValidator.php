1e39b8a6e9e94a284bf99642335f5975c09453eb
denshade <lieven.vaneeckhaute@gmail.com>
2013-01-10 17:34:51 +0100

    New site

---
+++ IControllerValidator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: denshade
+ * Date: 1/10/13
+ * Time: 1:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class IControllerValidator
+{
+
+}
---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head><title>Schemas and examples</title></head>
+<style type="text/css">
+body {
+    background: black url('back.jpg') center;
+    color: #f0ffff;
+}
+.listingDownloads
+{
+    text-align: center
+}
+
+.listingDownloads a
+{
+    color: #fff8dc;
+}
+</style>
+<body>
+<div class="listingDownloads">
+<p>
+   <a href="example.xml">An example import xml.</a></p>
+   <a href="icontroller-import.xsd">Our import xsd.</a>
+</div>
+
+</body>
+
+</html>
+
